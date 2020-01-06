feca86997b03977244e3ef421b39e63d99b7f321
oyelowo <oyelowo.oyedayo@relex.fi>
2020-01-06 23:35:14 +0200

    Removed old implementation

--- packages/redux-fetch/utils/shared.ts
+++
@@ -1,8 +0,0 @@
-export const getApiActions = (type: string) => ({
-  request: `${type}_REQUEST`,
-  success: `${type}_SUCCESS`,
-  failure: `${type}_FAILURE`,
-  clearData: `${type}_DELETE`,
-})
-
-export type ApiActions = ReturnType<typeof getApiActions>
