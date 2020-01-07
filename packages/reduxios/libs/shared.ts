0c53642573f6b152fe03a69e2d75457cdeb9d953
oyelowo <oyelowo.oyedayo@relex.fi>
2020-01-07 20:12:57 +0200

    Added readme

--- packages/reduxios/libs/shared.ts
+++ packages/reduxios/libs/shared.ts
@@ -2,7 +2,7 @@ export const getApiActions = (type: string) => ({
   request: `${type}_REQUEST`,
   success: `${type}_SUCCESS`,
   failure: `${type}_FAILURE`,
-  resetState: `${type}_RESET`,
+  resetState: `${type}_RESET_STATE`,
 })
 
 export type ApiActions = ReturnType<typeof getApiActions>
